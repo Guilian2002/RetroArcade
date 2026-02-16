b2b55ce18e34c4f83a198841951df06da4f6f716
Pierard Guilian <guiliangp07@gmail.com>
2026-02-16 11:29:23 +0100

    Add booking entity in domain and add new stored procedure for date verification

---
+++ RetroArcade.DB/StoredProcedures/SP_Booking_Get_By_Room.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[SP_Booking_Get_By_Room]
+	@roomId UNIQUEIDENTIFIER
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		[Id],
+		[BookingDate],
+		[BeginHour],
+		[EndHour],
+		[GroupSize],
+		[Price],
+		[Status],
+		[RoomId]
+	FROM [dbo].[Booking]
+	WHERE [RoomId] = @roomId;
+END
---
+++ RetroArcade.Domain/Domain/Entities/Booking.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroArcade.Domain.Domain.Entities
+{
+    public class Booking
+    {
+        #region Attributes
+        public Guid Id { get; set; }
+        public DateTime BookingDate { get; set; }
+        public TimeSpan BeginHour { get; set; }
+        public TimeSpan EndHour { get; set; }
+        public int GroupSize { get; set; }
+        public Status Status { get; set; }
+        public Decimal Price { get; set; }
+        public Account? Account { get; set; }
+        public Room? Room { get; set; }
+        #endregion
+
+        #region CTOR
+        public Booking(Guid id, DateTime bookingDate, TimeSpan beginHour, TimeSpan endHour,
+            int groupSize, Status status, decimal price, Account? account, Room? room)
+        {
+            Id = id;
+            BookingDate = bookingDate;
+            BeginHour = beginHour;
+            EndHour = endHour;
+            GroupSize = groupSize;
+            Status = status;
+            Price = price;
+            Account = account;
+            Room = room;
+        }
+        #endregion
+
+        #region Methods
+        public bool VerifyGroupSizeAllowed() => GroupSize > 0 && GroupSize <= Room?.MachineCapacity;
+        #endregion
+    }
+}
